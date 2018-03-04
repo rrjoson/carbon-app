42eb731e358eba636abb2eb717ef0c980db0ac9e
rrjoson <rrjoson08@gmail.com>
2018-03-04 19:06:27 +0800

    :fire: Remove badge component

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ import Link from './Link';
 import Card from './Card';
 import Button from './Button';
 import Header from './Header';
-import Badge from './Badge';
 import Status from './Status';
 import Filter from './Filter';
 import Table from './Table';
@@ -15,7 +14,6 @@ export {
   Card,
   Button,
   Header,
-  Badge,
   Status,
   Filter,
   Table,

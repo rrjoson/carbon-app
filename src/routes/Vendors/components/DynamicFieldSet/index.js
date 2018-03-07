dd83071fa79f008091ffc41fad4ab5af60b3f1f4
rrjoson <rrjoson08@gmail.com>
2018-03-07 22:20:34 +0800

    :speech_balloon: Update vendor page button copy

--- src/routes/Vendors/components/DynamicFieldSet/index.js
+++ src/routes/Vendors/components/DynamicFieldSet/index.js
@@ -114,7 +114,7 @@ class DynamicFieldSet extends Component {
                   {formItems}
                   <FormItem {...formItemLayoutWithOutLabel}>
                     <Button onClick={() => this.add(vendor.name)} style={{ width: '132px' }}>
-                      <Icon type="plus" /> Add Product
+                      <Icon type="plus" /> Add Vendor
                     </Button>
                   </FormItem>
                 </Col>

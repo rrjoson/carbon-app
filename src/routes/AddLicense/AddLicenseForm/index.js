455139c494f089d1b1919db341bb1dffd2fe985d
rrjoson <rrjoson08@gmail.com>
2018-03-15 00:15:30 +0800

    :children_crossing: Update license page level of support UI

--- src/routes/AddLicense/AddLicenseForm/index.js
+++ src/routes/AddLicense/AddLicenseForm/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input, Icon, Button, Row, Col, DatePicker, Select, TimePicker, Radio } from 'antd';
+import { Input, Icon, Button, Row, Col, DatePicker, Select, Radio } from 'antd';
 
 import { Link, Typography, Form } from './../../../components';
 
@@ -14,13 +14,22 @@ class DynamicFieldSet extends Component {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        this.props.onSave(values);
+        const data = Object.assign({}, values);
+
+        if (values.on_site_other) {
+          data.on_site = values.on_site_other;
+          delete data.on_site_other;
+        }
+
+        this.props.onSave(data);
       }
     });
   }
 
   render() {
     const { getFieldDecorator, getFieldValue } = this.props.form;
+
+    const onSite = getFieldValue('on_site');
 
     const formItemLayout = {
       labelCol: {
@@ -150,21 +159,46 @@ class DynamicFieldSet extends Component {
         </Row>
 
         <Row gutter={12}>
-          <Col span={3}>
+          <Col span={24}>
             <FormItem label="On-site">
               {getFieldDecorator('on_site', {
+                initialValue: '8x5',
                 rules: [{
                   required: true,
                   message: 'This is a required field',
                 }],
               })(
-                <Select placeholder={''}>
-                  <Option value={'8x5'}>8x5</Option>
-                </Select>
+                <Radio.Group>
+                  <Radio.Button value="8x5">8x5</Radio.Button>
+                  <Radio.Button value="24x7">24x7</Radio.Button>
+                  <Radio.Button value="12x6">12x6</Radio.Button>
+                  <Radio.Button value="Other">Other</Radio.Button>
+                </Radio.Group>
               )}
             </FormItem>
           </Col>
+        </Row>
 
+        {
+          (onSite === 'Other')
+          ? <Row gutter={12}>
+              <Col span={3}>
+                <FormItem label="On-site">
+                  {getFieldDecorator('on_site_other', {
+                    rules: [{
+                      required: true,
+                      message: 'This is a required field',
+                    }],
+                  })(
+                    <Input />
+                  )}
+                </FormItem>
+              </Col>
+            </Row>
+          : null
+        }
+
+        <Row gutter={12}>
           <Col span={3}>
             <FormItem label="Date Start">
               {getFieldDecorator('support_date_start', {

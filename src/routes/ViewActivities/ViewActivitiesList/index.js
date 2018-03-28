534b135cec47f14b2ae385f89ed023ddd2b5e143
rrjoson <rrjoson08@gmail.com>
2018-03-28 18:57:50 +0800

    :sparkles: Add SE activities page

--- src/routes/ViewActivities/ViewActivitiesList/index.js
+++ src/routes/ViewActivities/ViewActivitiesList/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Typography, Link, Button, Activity } from './../../../components';
+
 import styles from './styles.css';
 
-function ViewActivitiesList(props) {
-  return (
-    <div className={styles.wrapper}>
-      List of Activities
-    </div>
-  );
-}
+const { H2 } = Typography;
 
-ViewActivitiesList.propTypes = {};
+class ViewActivitiesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: {},
+    };
+  }
+
+  handleClick = (item, index) => {
+    const { selected } = this.state;
+
+    if (selected[index]) {
+      delete selected[index];
+    } else {
+      selected[index] = item;
+    }
+
+    this.setState({ selected });
+  }
+
+  render() {
+    const {
+      data,
+    } = this.props;
+
+    return (
+      <div className={styles.viewActivitiesList}>
+        {
+          data.map((item, index) => {
+            return (
+              <Activity
+                item={item}
+                glocalId={item.glocalId}
+                onClick={() => this.handleClick(item, index)}
+                open={this.state.selected[index]}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
 
 export default ViewActivitiesList;

7e7baa6df5598afbd8b166d63c90282652c63ebf
rrjoson <rrjoson08@gmail.com>
2018-04-12 01:21:52 +0800

    :speech_balloon: Update error page copy

--- src/routes/Error/index.js
+++ src/routes/Error/index.js
@@ -6,13 +6,14 @@ import styles from './styles.css';
 import notFound from '../../assets/not-found.svg';
 
 import { H1, H4 } from './../../components/Typography';
+import Button from './../../components/Button';
 
 const Error = () => (
   <div className={styles.error}>
     <img className={styles.image} src={notFound} />
     <div className={styles.text}>
       <H1>404</H1>
-      <H4>Page not found</H4>
+      <H4>Sorry, the page you are looking for could not be found.</H4>
     </div>
   </div>
 );

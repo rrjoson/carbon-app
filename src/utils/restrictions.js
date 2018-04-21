34c91b7286bd84959756638216e908045a7f190a
rrjoson <rrjoson08@gmail.com>
2018-04-21 18:42:07 +0800

    :sparkles: Update restrictions

--- src/utils/restrictions.js
+++ src/utils/restrictions.js
@@ -1,5 +1,32 @@
 export const restrictions = {
-  'Director': [],
+  'Managing Director': [
+    'EDIT_CASE',
+  ],
+  'Sales Director': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
   'Sales Manager': [
     'ADD_CASE',
     'EDIT_CASE',
@@ -10,9 +37,92 @@ export const restrictions = {
     'ADD_ACTIVITY',
     'EXPORT_ACTIVITY',
     'UPDATE_ACTIVITY',
-
     'DELETE_ACTIVITY',
+
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
     'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Senior Sales Consultant': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Sales Consultant': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Senior Account Manager': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
     'DELETE_VENDOR',
 
     'ADD_USER',
@@ -29,7 +139,117 @@ export const restrictions = {
     'UPDATE_ACTIVITY',
     'DELETE_ACTIVITY',
 
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
     'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Product Specialist': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'DELETE_PRODUCT',
+
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Business Development Director': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'DELETE_CLIENT',
+    'DELETE_LICENSE',
+
+    'DELETE_PRODUCT',
+
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Corporate Affairs Director': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'ADD_CLIENT',
+    'UPDATE_CLIENT',
+    'DELETE_CLIENT',
+
+    'ADD_LICENSE',
+    'UPDATE_LICENSE',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Project Manager': [
+    'ADD_CASE',
+    'EDIT_CASE',
+    'DELETE_CASE',
+
+    'ADD_ASSIGNED_SE',
+
+    'ADD_ACTIVITY',
+    'EXPORT_ACTIVITY',
+    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'ADD_CLIENT',
+    'UPDATE_CLIENT',
+    'DELETE_CLIENT',
+
+    'ADD_LICENSE',
+    'UPDATE_LICENSE',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
     'DELETE_VENDOR',
 
     'ADD_USER',
@@ -37,7 +257,52 @@ export const restrictions = {
   'Technical Manager': [
     'DELETE_CASE',
 
-    'UPDATE_ACTIVITY',
+    'DELETE_ACTIVITY',
+
+    'ADD_CLIENT',
+    'UPDATE_CLIENT',
+    'DELETE_CLIENT',
+
+    'ADD_LICENSE',
+    'UPDATE_LICENSE',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Team Lead': [
+    'DELETE_CASE',
+
+    'DELETE_ACTIVITY',
+
+    'ADD_CLIENT',
+    'UPDATE_CLIENT',
+    'DELETE_CLIENT',
+
+    'ADD_LICENSE',
+    'UPDATE_LICENSE',
+    'DELETE_LICENSE',
+
+    'ADD_PRODUCT',
+    'EDIT_PRODUCT',
+    'DELETE_PRODUCT',
+
+    'ADD_VENDOR',
+    'EDIT_VENDOR',
+    'DELETE_VENDOR',
+
+    'ADD_USER',
+  ],
+  'Senior Systems Engineer': [
+    'DELETE_CASE',
+
     'DELETE_ACTIVITY',
 
     'ADD_CLIENT',
@@ -81,5 +346,5 @@ export const restrictions = {
     'DELETE_VENDOR',
 
     'ADD_USER',
-  ],
+  ]
 };
